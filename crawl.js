1056e889c0ebbe0a147d1dd1f99fbca0cc74fd18
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-11-30 08:42:46 +0530

    Crawl single page

--- crawl.js
+++ crawl.js
@@ -1,21 +1,37 @@
-const { JSDOM } = require("jsdom");
+const { JSDOM } = require('jsdom');
 
-async function crawlPage(currentURL){
-    console.log(`Actively crawling ${currentURL}`);
+async function crawlPage(currentURL) {
+  console.log(`Actively crawling ${currentURL}`);
+  try {
+    const res = await fetch(currentURL);
 
-    const res = await fetch(currentURL)
-
-    console.log(await res.text());
+    if (res.status > 399) {
+      console.log(
+        `Error is fetching with status code: ${res.status} on page ${currentURL}`
+      );
+      return;
+    }
+    const contentType = res.headers.get('content-type');
+    if (!contentType.includes('text/html')) {
+      console.log('Non html response :' + contentType);
+      return 
+    }
+    const htmlBody = await res.text();
+    const linkElements = getURLsFromHTML(htmlBody, currentURL);
+    for (const linkElement of linkElements) {
+    }
+  } catch (err) {
+    console.log(`Error while fetching the URL: ${currentURL}`);
+  }
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
-  const linkElements = dom.window.document.querySelectorAll("a");
-  
+  const linkElements = dom.window.document.querySelectorAll('a');
 
   for (const linkElement of linkElements) {
-    if (linkElement.href.slice(0, 1) === "/") {
+    if (linkElement.href.slice(0, 1) === '/') {
       try {
         const urlObj = new URL(`${baseURL}${linkElement.href}`);
         urls.push(urlObj.href);
@@ -23,7 +39,6 @@ function getURLsFromHTML(htmlBody, baseURL) {
         console.log(`error with relative url: ${error.message}`);
       }
     } else {
-     
       try {
         const urlObj = new URL(`${linkElement.href}`);
         urls.push(urlObj.href);
@@ -40,7 +55,7 @@ function normalizeURL(urlString) {
   const newObj = new URL(urlString);
   const hostPath = `${newObj.hostname}${newObj.pathname}`;
 
-  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+  if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
     return hostPath.slice(0, -1);
   }
 
@@ -50,5 +65,5 @@ function normalizeURL(urlString) {
 module.exports = {
   normalizeURL,
   getURLsFromHTML,
-  crawlPage
+  crawlPage,
 };
